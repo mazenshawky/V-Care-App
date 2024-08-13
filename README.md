36ba7d3dbf1b87803af6e230cf34e795aa6b27d6
mazenshawky <mazenshawqy@gmail.com>
2024-08-13 15:26:22 +0300

    Initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# v_care_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/v_care_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.v_care_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
